ec8d4b70ef95579fdcf63aa2cf27a6ada01917cd
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2022-05-07 13:11:05 +0000

    Add library files

---
+++ ei/third_party/arc_mli_package/include/mli_api.h
@@ -0,0 +1,24 @@
+/*
+* Copyright 2019-2020, Synopsys, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-3-Clause license found in
+* the LICENSE file in the root directory of this source tree.
+*
+*/
+
+/**
+ * @file MLI Library API
+ *
+ * @brief This header includes all necessary files for using MLI Library
+ */
+
+#ifndef _MLI_API_H_
+#define _MLI_API_H_
+
+#include <ei/third_party/arc_mli_package/include/api/mli_helpers_api.h>
+#include <ei/third_party/arc_mli_package/include/api/mli_kernels_api.h>
+#include <ei/third_party/arc_mli_package/include/api/mli_mov_api.h>
+#include <ei/third_party/arc_mli_package/include/mli_types.h>
+
+#endif //#ifndef _MLI_API_H_
